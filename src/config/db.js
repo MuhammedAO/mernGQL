b1812e394d078788a5d21047b472cacc37a5ac2a
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-14 00:26:49 +0100

    connectDB

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import { mongoURI } from "./keys.js"
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(mongoURI, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    console.log(`database connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+  }
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import express from "express"
 import { ApolloServer } from "apollo-server-express"
 import typeDefs from "./typeDefs/index.js"
 import resolvers from "./resolvers/index.js"
+import connectDB from "./config/db.js"
 
-const { PORT = 4000, NODE_ENV = "development" } = process.env
+const { PORT = 4000 } = process.env
+
+connectDB()
 
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers, playground: true })
   await server.start()
 
   const app = express()
-  app.disable('x-powered-by')
-  
+  app.disable("x-powered-by")
+
   server.applyMiddleware({ app })
 
   await new Promise((resolve) => app.listen({ port: PORT }, resolve))
