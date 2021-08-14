90ae7d93008f07bfbbfc9a3f7ef2136d05fcea5f
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-14 02:00:34 +0100

    create user

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,10 +1,28 @@
+import mongoose from "mongoose"
+import { UserInputError } from "apollo-server-express"
+import { User } from "../models/index.js"
+
 export default {
+  /*Apollo handles promises rejection and resolution on its own */
   Query: {
-    users: (parent, context, args, info) => {},
-    user: (parent, context, args, info) => {},
+    users: (parent, args, context, info) => {
+      //Todo: Auth, Projection, Pagination, Sanitization
+      return User.find({})
+    },
+    user: (parent, { id }, context, info) => {
+      //Todo: Auth, Projection, Pagination, Sanitization
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new UserInputError(`${id} is not a valid userId`)
+      }
+      return User.findById(id)
+    },
   },
 
   Mutation: {
-    signUp: (parent, context, args, info) => {},
+    signUp: (parent, args, context, info) => {
+      //Todo: !Auth, Validation
+
+      return User.create(args)
+    },
   },
 }
