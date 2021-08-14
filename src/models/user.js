b185ce3319049bf6d6fc340668da7c5edd03c94f
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-14 02:00:53 +0100

    bcryptjs

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,27 @@
 import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
 
 const userSchema = new mongoose.Schema(
   {
     email: String,
     username: String,
     name: String,
-    password: String
+    password: String,
   },
   {
     timestamps: true,
   }
 )
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(10)
+      this.password = await bcrypt.hash(this.password, salt)
+    } catch (error) {
+      next(error)
+    }
+  }
+})
 
 export default mongoose.model("User", userSchema)
