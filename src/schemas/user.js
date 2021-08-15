4531376b60eee635c90b555ed44ebd005d37cb2a
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-15 06:53:17 +0100

    schema validation for signin

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -5,6 +5,21 @@ export const schema = Joi.object().keys({
   username: Joi.string().alphanum().min(4).max(30).required().label("Username"),
   name: Joi.string().max(254).required().label("Name"),
   password: Joi.string()
+    .min(8)
+    .max(30)
+    .pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,30}$/)
+    .label("Password")
+    .messages({
+      "string.pattern.base":
+        "Password must have at least one uppercase letter, one lowercase letter, one digit, and one special character",
+    }),
+})
+
+export const schema1 = Joi.object().keys({
+  email: Joi.string().email().required().label("Email"),
+  password: Joi.string()
+    .min(8)
+    .max(30)
     .pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,30}$/)
     .label("Password")
     .messages({
