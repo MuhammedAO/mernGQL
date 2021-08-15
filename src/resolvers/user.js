b5bcb2ef54f69087348df23560bea03b3d872e26
Muhammed ogunsanya <mspeaks910@gmail.com>
2021-08-15 06:55:37 +0100

    user authentication

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,17 +1,31 @@
 import mongoose from "mongoose"
 import { UserInputError } from "apollo-server-express"
 import { User } from "../models/index.js"
-import { schema } from "../schemas/user.js"
+import { schema, schema1 } from "../schemas/user.js"
+// import { signUp, signIn } from "../schemas/user.js"
+import * as Auth from "../auth.js"
 
 export default {
   /*Apollo handles promises rejection and resolution on its own */
   Query: {
-    users: (parent, args, context, info) => {
+    me: (parent, args, { req }, info) => {
+      //Todo: Projection
+
+      Auth.checkSignedIn(req)
+
+      return User.findById(req.session.userId)
+    },
+    users: (parent, args, { req }, info) => {
       //Todo: Auth, Projection, Pagination, Sanitization
+
+      Auth.checkSignedIn(req)
+
       return User.find({})
     },
-    user: (parent, { id }, context, info) => {
+    user: (parent, { id }, { req }, info) => {
       //Todo: Auth, Projection, Pagination, Sanitization
+
+      Auth.checkSignedIn(req)
       if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new UserInputError(`${id} is not a valid userId`)
       }
@@ -20,12 +34,36 @@ export default {
   },
 
   Mutation: {
-    signUp: async (parent, args, context, info) => {
+    signUp: async (parent, args, { req }, info) => {
       //Todo: !Auth, Validation
-
+      Auth.checkSignedOut(req)
       await schema.validateAsync(args, { abortEarly: false })
 
-      return User.create(args)
+      const user = await User.create(args)
+
+      req.session.userId = user.id
+
+      return user
+    },
+
+    signIn: async (parent, args, { req }, info) => {
+      const { userId } = req.session
+      if (userId) {
+        return User.findById(userId)
+      }
+      await schema1.validateAsync(args, { abortEarly: false })
+
+      const { email, password } = args
+
+      const user = await Auth.attemptSignIn(email, password)
+      req.session.userId = user.id
+      return user
+    },
+
+    signOut: (parent, args, { req, res }, info) => {
+      Auth.checkSignedIn(req)
+
+      return Auth.signOut(req, res)
     },
   },
 }
